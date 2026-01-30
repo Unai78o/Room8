a60bbf442eacb6ee18b3c7c42c4411e4980d2fb7
Unai78o <unailobetepp@gmail.com>
2026-01-30 22:00:51 +0100

    SideBar Button Working. Sidebar missing.

--- components/SidebarButton.tsx
+++ components/SidebarButton.tsx
@@ -3,12 +3,20 @@ import { LucideIcon } from "lucide-react";
 interface Props {
     icon: LucideIcon;
     isActive?: boolean;
+    onClick?: () => void;
 }
 
-export function SidebarButton({ icon: Icon, isActive = false }: Props) {
+export function SidebarButton({ icon: Icon, isActive = false, onClick }: Props) {
     return (
-        <div className="relative flex items-center justify-center group">
-            
-        </div>
+        <button
+        onClick={onClick}
+        className={`relative flex items-center justify-center group w-3/4 h-full cursor-pointer text-gray-400 hover:text-white transition-colors  ${
+            isActive && 'text-white'
+        }`}>
+            {isActive
+                && <div className='absolute left-0 top-0 bottom-0 bg-indigo-500 w-1 h-auto rounded-r-md'></div>
+            }
+            <Icon size={24} />
+        </button>
     );
 }
